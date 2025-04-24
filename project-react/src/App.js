592317663af7e8b2e1aa8b22d478e280f5eb5bd9
jsThanuja <jsthanuja23@gmail.com>
2025-04-24 11:20:46 +0530

    increment by even numbers

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -5,12 +5,14 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome.js'
 import Hello from  './components/Hello'
 import Message from './components/Message.js'
+import Counter from './components/Counter.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message/> {/*
+         <Counter/>
+      {/*  <Message/> 
       <Greet name="thanu" heroName = "Thanuja"> 
       <p> This is My Name </p>
       </Greet>
