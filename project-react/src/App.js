5a2a6230270ae4ba49009d312b7a4864333cfd23
jsThanuja <jsthanuja23@gmail.com>
2025-04-24 09:53:20 +0530

    onclick action of button

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome.js'
 import Hello from  './components/Hello'
+import Message from './components/Message.js'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Message/> {/*
       <Greet name="thanu" heroName = "Thanuja"> 
       <p> This is My Name </p>
       </Greet>
